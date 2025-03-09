b606555606f2a548632eb3b23ff238fc12046165
KarthikeyanRV2601 <karthikradha06@gmail.com>
2025-03-09 18:21:29 +0530

    v(2.0.0): netlify api functions deploy fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,3 @@
-import Head from "next/head";
-import App from "./_app";
-
-export default function Index() {
-
-  return (
-    <html lang="en">
-      <Head>
-        <link rel="icon" href="./favicon.ico"/>
-        <title>Policy Compliance System</title>
-      </Head>
-      <body>
-        <App />
-      </body>
-    </html>
-  );
+export default function Home() {
+  return <h1>Welcome to My Next.js App!</h1>;
 }
